1a6fd180ba6ada4af7ecf70b9ced6b58242738e0
Felipe <felipeshimizu5@gmail.com>
2026-06-24 13:34:55 -0300

    feat: refina cards de jogos e exibição de plataformas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -256,7 +256,7 @@ export default function Home() {
           </div>
         </div>
       ) : (
-        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4">
           {jogos.map((jogo, i) => (
             <div
               key={jogo.id}
